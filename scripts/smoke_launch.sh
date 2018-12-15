ec519e415ffe88eac59ec6e43602db1a01d84065
parakram <appu226@yahoo.co.in>
2018-12-15 08:37:19 +0000

    create and delete temp directory

--- scripts/smoke_launch.sh
+++ scripts/smoke_launch.sh
@@ -2,8 +2,10 @@ set -e
 make -s -j 4
 factor_graph_main/factor_graph_main | sed "s/^/[factor_graph_main] /"
 qbf_solve/qbf_solve | sed "s/^/[qbf_solve] /"
+mkdir -p temp
 blif_solve/blif_solve --verbosity DEBUG --factor_graph --cudd test/data/simple_and.blif | grep -v sec | tee temp/simple_and.out | sed "s/^/[blif_solve] /"
 diff temp/simple_and.out test/expected_outputs/simple_and.out
+rm -rf temp
 echo "[blif_solve diff] SUCCESS"
 test/test | sed "s/^/[test] /"
 echo
