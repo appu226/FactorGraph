cac96c6fdccf719de2106c9ac3ad0b73364800a2
parakram <appu226@yahoo.co.in>
2019-02-10 16:37:21 +0000

    make model counting optional

--- scripts/smoke/blif_solve.sh
+++ scripts/smoke/blif_solve.sh
@@ -4,7 +4,7 @@ echo creating temp
 mkdir -p temp
 
 echo comparing ExactAndAccumulate with FactorGraphAprox
-blif_solve/blif_solve --verbosity DEBUG --under_approximating_method ExactAndAccumulate --over_approximating_method FactorGraphApprox --diff_output_path temp/simple_and.dimacs test/data/simple_and.blif | grep -v sec > temp/simple_and.out
+blif_solve/blif_solve --verbosity DEBUG --under_approximating_method ExactAndAccumulate --over_approximating_method FactorGraphApprox --must_count_solutions --diff_output_path temp/simple_and.dimacs test/data/simple_and.blif | grep -v sec > temp/simple_and.out
 (exit ${PIPESTATUS[0]})
 
 echo diff temp/simple_and.out and test/expected_outputs/simple_and.out
