a99b45724fdf57347adb189b9d2e22735a3e802f
parakram <appu226@yahoo.co.in>
2020-02-02 11:42:34 +0000

    run Cudd_bddAndAbstractMulti tests with cache

--- experiments/Cudd_BddAndAbstractMulti/run_all_test_cases.sh
+++ experiments/Cudd_BddAndAbstractMulti/run_all_test_cases.sh
@@ -10,11 +10,11 @@ summary_file=${results_dir}/summary.txt
 
 for test_case in $(ls ${test_cases_dir}/*.blif); do
   test_case_name=$(basename ${test_case} .blif)
-  command="blif_solve/blif_solve --under_approximating_method Skip --over_approximating_method ExactAndAbstractMulti --must_count_solutions --verbosity INFO ${test_case}"
+  command="blif_solve/blif_solve --under_approximating_method Skip --over_approximating_method ExactAndAbstractMulti --must_count_solutions --verbosity INFO --cacheSize 100000 ${test_case}"
   echo Running ${test_case_name} | tee -a ${log_file}
   (timeout 2m ${command}) | tee -a ${log_file}
   echo Finished ${test_case_name}
 done
 
 g++ ${base_dir}/process_logs.cpp -o ${base_dir}/process_logs
-process_logs <${log_file} | tee ${summary_file}
+${base_dir}/process_logs <${log_file} | tee ${summary_file}
