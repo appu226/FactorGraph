27827273bff033879dbeeb3d56811eda581c8024
parakram <appu226@yahoo.co.in>
2020-08-19 00:10:46 +0100

    Dummy command line options for var_score

--- var_score/main.cpp
+++ var_score/main.cpp
@@ -1,9 +1,77 @@
-#include <iostream>
+/*
+
+Copyright 2020 Parakram Majumdar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 
 
 
-int main()
+#include <log.h>
+#include <command_line_options.h>
+
+
+#include <memory>
+
+
+template<typename TValue>
+std::shared_ptr<blif_solve::CommandLineOptionValue<TValue> > 
+  addCommandLineOption(std::vector<std::shared_ptr<blif_solve::ICommandLineOption> > & commandLineOptions,
+                       const std::string & name,
+                       const std::string & help,
+                       const TValue & defaultValue);
+
+
+
+
+int main(int argc, char const * const * const argv)
 {
-  std::cout << "Hello world" << std::endl;
+  std::vector<std::shared_ptr<blif_solve::ICommandLineOption> > clo;
+  auto verbosity = addCommandLineOption<std::string>(clo, "--verbosity", "verbosity (QUIET/ERROR/WARNING/INFO/DEBUG)", "WARNING");
+  auto blif = addCommandLineOption<std::string>(clo, "--blif", "blif file name", "");
+
+
+  parseCommandLineOptions(argc - 1, argv + 1, clo);
+  blif_solve::setVerbosity(blif_solve::parseVerbosity(verbosity->getValue()));
+  if (blif->getValue() == "")
+  {
+    blif_solve_log(ERROR, "Expecting value for command line argument --blif");
+    blif_solve::printHelp(clo);
+    return -1;
+  }
+  blif_solve_log(DEBUG, "blif file: " + blif->getValue());
+
   return 0;
+}
+
+
+
+
+template<typename TValue>
+std::shared_ptr<blif_solve::CommandLineOptionValue<TValue> >
+  addCommandLineOption(std::vector<std::shared_ptr<blif_solve::ICommandLineOption> > & commandLineOptions,
+                       const std::string & name,
+                       const std::string & help,
+                       const TValue & defaultValue)
+{
+  auto result = blif_solve::CommandLineOptionValue<TValue>::create(name, help, defaultValue);
+  commandLineOptions.push_back(result);
+  return result;
 }
