150e52193242ce211a101aabff71c637ec71d841
parakram <appu226@yahoo.co.in>
2020-11-17 13:00:09 +0000

    upload test results and write-up

--- experiments/var_score/find_parseable_but_unsolvable_cases/run_all_test_cases.sh
+++ experiments/var_score/find_parseable_but_unsolvable_cases/run_all_test_cases.sh
@@ -1,6 +1,6 @@
 basedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 outdir=$basedir/results
-timeout=10s
+timeout=5m
 mkdir -p $outdir
 for test_case in $($basedir/collect_test_cases.sh); do
   $basedir/run_one_test_case.sh $basedir $test_case $outdir $timeout
