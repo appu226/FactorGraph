ef0608201c7d57b4e5b696da7f96bc310624e60d
Parakram Majumdar <appu226@yahoo.co.in>
2022-01-09 21:56:08 -0500

    integrate mustool into dec_21

--- dec_21/main.cpp
+++ dec_21/main.cpp
@@ -35,9 +35,12 @@ SOFTWARE.
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 #include <cudd.h>
 #include <cuddInt.h>
+
+#include <mustool/core/Master.h>
 
 
 // struct to contain parse command line options
@@ -49,6 +52,21 @@ struct CommandLineOptions {
 
 
 
+struct DummyMucCallback: public MucCallback
+{
+    void processMuc(const std::vector<std::vector<int> >& muc) override
+    {
+      std::cout << "Callback found an MUC:\n";
+      for(const auto & clause: muc) {
+        for (int v: clause)
+          std::cout << v << " ";
+        std::cout << "\n";
+      }
+    }
+};
+
+
+
 
 // function declarations
 CommandLineOptions parseClo(int argc, char const * const * const argv);
@@ -56,6 +74,7 @@ int main(int argc, char const * const * const argv);
 std::shared_ptr<DdManager> ddm_init();
 factor_graph * createFactorGraph(DdManager* ddm, const dd::QdimacsToBdd& qdimacsToBdd);
 std::shared_ptr<dd::Qdimacs> parseQdimacs(const std::string & inputFilePath);
+std::shared_ptr<Master> createMustMaster(const dd::Qdimacs& qdimacs);
 
 
 
@@ -81,11 +100,14 @@ int main(int argc, char const * const * const argv)
       << qdimacs->clauses.size() << " clauses in "
       << blif_solve::duration(start) << " sec");
 
-  auto numIterations = factor_graph_converge(fg);
+  auto numIterations = factor_graph_converge(fg);                      // converge factor graph
   blif_solve_log(INFO, "Factor graph converged after " 
       << numIterations << " iterations in "
       << blif_solve::duration(start) << " secs");
+  
 
+  auto mustMaster = createMustMaster(*qdimacs);
+  mustMaster->enumerate();
 
   // cleanup
   if (fg) factor_graph_delete(fg);
@@ -183,3 +205,45 @@ std::shared_ptr<DdManager> ddm_init()
   return m;
 }
 
+
+
+
+
+
+// initialize must
+std::shared_ptr<Master> createMustMaster(const dd::Qdimacs& qdimacs)
+{
+  // check that we have exactly one quantifier which happens to be existential
+  assert(qdimacs.quantifiers.size() == 1);
+  assert(qdimacs.quantifiers.front().quantifierType == dd::Quantifier::Exists);
+  const auto & quantifiedVariablesVec = qdimacs.quantifiers.front().variables;
+  std::set<int> quantifiedVariableSet(quantifiedVariablesVec.cbegin(), quantifiedVariablesVec.cend());
+  auto isQuantifiedVariable = [&](int v)-> bool { return quantifiedVariableSet.count(v) + quantifiedVariableSet.count(-v) > 0; };
+
+  // filter out clauses with at least one quantified variable
+  // remove all the quantified variables
+  std::vector<std::vector<int> > outputClauses;
+  std::set<std::set<int> > uniqueOutputClauses;
+  for (const auto & inputClause : qdimacs.clauses)
+  {
+    std::vector<int> outputClause;
+    std::copy_if(inputClause.cbegin(), inputClause.cend(), std::back_inserter(outputClause), isQuantifiedVariable);
+    std::set<int> uniqueOutputClause(outputClause.cbegin(), outputClause.cend());
+    if (!outputClause.empty() && !uniqueOutputClauses.count(uniqueOutputClause))
+    {
+      uniqueOutputClauses.insert(uniqueOutputClause);
+      outputClauses.push_back(outputClause);
+    }
+  }
+  auto result = std::make_shared<Master>(qdimacs.numVariables, outputClauses, "remus");
+  // result->verbose = ??;
+  result->depthMUS = 6;
+  result->dim_reduction = 0.9;
+  result->validate_mus_c = true;
+  result->satSolver->shrink_alg = "default";
+  result->get_implies = true;
+  result->criticals_rotation = false;
+  auto mucCallback = std::make_shared<DummyMucCallback>();
+  result->setMucCallback(mucCallback);
+  return result;
+}
