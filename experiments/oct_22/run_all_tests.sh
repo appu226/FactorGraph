04979238a674e207b5c77a49506fa03ff20042ec
Parakram Majumdar <appu226@yahoo.co.in>
2022-11-01 08:03:47 +0530

    early validation of environment

--- experiments/oct_22/run_all_tests.sh
+++ experiments/oct_22/run_all_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ -z ${TIME_OUT+x} ]; then echo Error: env var TIME_OUT needs to be set to number of seconds; exit 1; fi
+if [ -z ${LARGEST_SUPPORT_SET+x} ]; then echo Error: env var LARGEST_SUPPORT_SET needs to be set; exit 1; fi
+if [ -z ${TEST_CASE_ROOT+x} ]; then echo Error: env var TEST_CASE_ROOT needs to be set to test case root folder; exit 1; fi
+if [ -z ${RESULT_ROOT+x} ]; then echo Error: env var RESULT_ROOT needs to be set to result root folder; exit 1; fi
 if [ -z ${TEST_PROCESS_COUNT+x} ]; then echo env var TEST_PROCESS_COUNT needs to be set to number of parallel test process.; exit 1; fi
 cat unsolvable.txt | xargs --max-procs=${TEST_PROCESS_COUNT} -I TEST_CASE ./run_test.sh TEST_CASE
--- experiments/oct_22/run_test.sh
+++ experiments/oct_22/run_test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-if [ -z ${TIME_OUT+x} ]; then echo Error: env var TIME_OUT needs to be set to number of seconds; exit 1; fi
-if [ -z ${TEST_CASE_ROOT+x} ]; then echo Error: env var TEST_CASE_ROOT needs to be set to test case root folder; exit 1; fi
-if [ -z ${RESULT_ROOT+x} ]; then echo Error: env var RESULT_ROOT needs to be set to result root folder; exit 1; fi
 test_name=$1
 export OUTPUT_PATH=${RESULT_ROOT}/${test_name}
 mkdir -p ${OUTPUT_PATH}
 echo $test_name
 ../../build/out/cnf_dump/simplify_qdimacs ${TEST_CASE_ROOT}/${test_name} ${OUTPUT_PATH}/simplified.qdimacs /dev/null
 ulimit -t ${TIME_OUT}
-../../build/out/oct_22/oct_22 --largestSupportSet 50 --inputFile ${OUTPUT_PATH}/simplified.qdimacs --verbosity INFO --outputFile ${OUTPUT_PATH}/result.cnf 1> ${OUTPUT_PATH}/run.log  2> ${OUTPUT_PATH}/run.err
+../../build/out/oct_22/oct_22 --largestSupportSet ${LARGEST_SUPPORT_SET} --inputFile ${OUTPUT_PATH}/simplified.qdimacs --verbosity INFO --outputFile ${OUTPUT_PATH}/result.cnf 1> ${OUTPUT_PATH}/run.log  2> ${OUTPUT_PATH}/run.err
 
