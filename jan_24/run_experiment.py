0365b44d15c7447fc6fc6c076f46a82e6abcd4b7
Parakram Majumdar <appu226@yahoo.co.in>
2024-01-28 23:38:28 -0500

    jan_24: fix bugs in run_experiments.py

--- jan_24/run_experiment.py
+++ jan_24/run_experiment.py
@@ -3,11 +3,15 @@ import argparse
 from dataclasses import dataclass
 from jan_24_common import LogLevelMapping
 import logging
-import multiprocessing
 import os
 import subprocess
 import sys
+from multiprocessing.pool import ThreadPool
 
+
+
+
+expt_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 
@@ -27,14 +31,14 @@ class CommandLineOptions:
     verbosity: str
     largest_bdd_size: str
     largest_support_set: str
-    factor_graph_timeout_seconds: str
+    factor_graph_timeout_seconds: int
     run_mus_tool: bool
     minimalize_assignments: bool
 
 
     def command(self: CommandLineOptions, test_case: str) -> list[str]:
             result: list[str] = [
-                "python", "build/out/jan_24/jan_24.py",
+                "python3", os.path.join(expt_root, "FactorGraph", "jan_24", "jan_24.py"),
                 "--test_case_path", os.path.join(self.input_folder, test_case),
                 "--output_root", self.output_folder,
                 "--run_bfss_preprocess", "1" if self.run_bfss_preprocess else "0",
@@ -43,8 +47,8 @@ class CommandLineOptions:
                 "--kissat_timeout_seconds", str(self.kissat_timeout_seconds),
                 "--preprocess_timeout_seconds", str(self.preprocess_timeout_seconds),
                 "--verbosity", self.verbosity,
-                "--factor_graph_bin", "build/out",
-                "--bfss_bin", "../bfss/bin",
+                "--factor_graph_bin", os.path.join(expt_root, "FactorGraph", "build", "out"),
+                "--bfss_bin", os.path.join(expt_root, "bfss", "bin"),
                 "--largest_bdd_size", str(self.largest_bdd_size),
                 "--largest_support_set", self.largest_support_set,
                 "--factor_graph_timeout_seconds", str(self.factor_graph_timeout_seconds),
@@ -61,13 +65,13 @@ class CommandLineOptions:
 
 def parse_filenames(argv: list[str]) -> list[str]:
     result: list[str] = []
-    for entry in argv[1:]:
-        file_path = os.path.join("jan_24", entry)
+    for entry in argv:
+        file_path = os.path.join(expt_root, "FactorGraph", "jan_24", entry)
         if os.path.exists(file_path) and os.path.isfile(file_path):
             with open(file_path) as fin:
                 result.extend([x.strip() for x in fin.readlines() if not x.strip() == "" and not x.strip().startswith("#")])
         else:
-            result.extend(entry)
+            result.append(entry)
     return result
 
 
@@ -79,9 +83,9 @@ def parse_args(argv: list[str]) -> CommandLineOptions:
     ap = argparse.ArgumentParser("run_experiment.py", description="Run multiple test cases")
     ap.add_argument("test_cases", metavar='T', type=str, nargs='+',
                     help="Individual qdimacs test case names, or filename with list of test cases.")
-    ap.add_argument("--input_folder", type=str, required=False, default="../all_test_cases", 
+    ap.add_argument("--input_folder", type=str, required=False, default=os.path.join(expt_root, "all_test_cases"),
                     help="Input folder with original qdimacs files.")
-    ap.add_argument("--output_folder", type=str, required=True, default="experiments/jan_24/results",
+    ap.add_argument("--output_folder", type=str, required=True, default=os.path.join(expt_root, "FactorGraph", "experiments", "jan_24", "results"),
                     help="Output folder to put results in")
     ap.add_argument("--num_processes", type=int, help="Number of processes to run in parallel.", required=False, default=os.cpu_count())
     ap.add_argument("--run_bfss_preprocess", type=bool, required=False, default=True,
@@ -111,26 +115,26 @@ def parse_args(argv: list[str]) -> CommandLineOptions:
     ap.add_argument("--minimalize_assignments", type=bool, required=False, default=True,
                     help="Whether to minimalize assignments found by MUST")
     args = ap.parse_args(argv[1:])
+    os.makedirs(args.output_folder, exist_ok=True)
     logging.basicConfig(
         format='[%(levelname)s] [%(asctime)s] %(message)s',
         level=LogLevelMapping[args.verbosity],
-        datefmt='%Y-%m-%d %H:%M:%S')
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[logging.StreamHandler(sys.stdout)])
     return CommandLineOptions(
         test_cases=parse_filenames(args.test_cases),
         input_folder=args.input_folder,
         output_folder=args.output_folder,
         num_processes=args.num_processes,
         run_bfss_preprocess=args.run_bfss_preprocess,
-        bfss_timeout_seconds=args.bfss_timeout_seconds,
+        bfss_timeout_seconds=int(args.bfss_timeout_seconds),
         run_kissat_preprocess=args.run_kissat_preprocess,
-        kissat_timeout_seconds=args.kissat_timeout_seconds,
-        preprocess_timeout_seconds=args.preprocess_timeout_seconds,
+        kissat_timeout_seconds=int(args.kissat_timeout_seconds),
+        preprocess_timeout_seconds=int(args.preprocess_timeout_seconds),
         verbosity=args.verbosity,
-        factor_graph_bin=args.factor_graph_bin,
-        bfss_bin=args.bfss_bin,
         largest_bdd_size=str(args.largest_bdd_size),
         largest_support_set=str(args.largest_support_set),
-        factor_graph_timeout_seconds=str(args.factor_graph_timeout_seconds),
+        factor_graph_timeout_seconds=int(args.factor_graph_timeout_seconds),
         run_mus_tool = args.run_mus_tool,
         minimalize_assignments = args.minimalize_assignments)
 
@@ -145,13 +149,16 @@ def run_test_case(input: tuple[str, CommandLineOptions]) -> int:
     log_file = os.path.join(clo.output_folder, test_case + ".log")
     sp = ' '
     logging.debug(f"Executing for {test_case}\n\tcommand = {sp.join(cmd)}\n\tlog_file={log_file}\n\terror={err_file}")
-    jan_24_process = subprocess.Popen(cmd, stdout=log_file, stderr=err_file)
+    with open(log_file, 'w') as log_handle:
+        with open(err_file, 'w') as err_handle:
+            jan_24_process = subprocess.Popen(cmd, stdout=log_handle, stderr=err_handle)
     return_code = -1
+    timeout = clo.preprocess_timeout_seconds + clo.factor_graph_timeout_seconds + 5
     try:
-        return_code = jan_24_process.wait(clo.preprocess_timeout_seconds + clo.factor_graph_timeout_seconds + 5)
+        return_code = jan_24_process.wait(timeout)
     except subprocess.TimeoutExpired:
         return_code = -1
-        logging.info(f"jan_24 timed out in {time_left} secs for {fng.factor_graph_input}")
+        logging.info(f"jan_24 timed out in {timeout} secs for {test_case}")
         jan_24_process.kill()
     return return_code
     
@@ -162,13 +169,15 @@ def run_test_case(input: tuple[str, CommandLineOptions]) -> int:
 
 def main(argv: list[str]) -> int:
     clo = parse_args(argv)
-    logging.info()
-
-    pool = multiprocessing.Pool(clo.num_processes)
-    ret_codes: list[int] = pool.apply(run_test_case, [(test_case, clo) for test_case in clo.test_cases])
+    logging.info("Starting jan_24/run_experiment.py")
+    logging.debug(f"Found {len(clo.test_cases)} test cases: {clo.test_cases}")
+    logging.debug(f"Initialising {clo.num_processes} threads")
+    pool = ThreadPool(clo.num_processes)
+    ret_codes: list[int] = pool.imap_unordered(run_test_case, [(test_case, clo) for test_case in clo.test_cases])
     for ret_code in ret_codes:
         if ret_code != 0:
             return ret_code
+    logging.info("Finished jan_24/run_experiment.py")
     return 0
 
 
